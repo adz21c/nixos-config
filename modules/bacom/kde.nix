b02b21c5e19bb5bfede7506c238fd3cf5f86fc18
Adam Burton <adz21c@gmail.com>
2019-09-30 21:14:03 +0100

    KDE desktop

--- modules/bacom/kde.nix
+++ modules/bacom/kde.nix
@@ -13,6 +13,7 @@ in {
   
   config = mkIf cfg.enable {
     services.xserver.desktopManager.plasma5.enable = true;
+	services.xserver.desktopManager.plasma5.enableQt4Support = true;
   
     environment.systemPackages = [
       ##############
